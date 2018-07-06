f1645e515715ee1bab646684339ef8bb388f2b0d
chengshenghao <1151799682@qq.com>
2018-07-06 16:11:05 +0800

    new:ORM框架room的使用

--- app/src/main/java/com/example/csh/androidarchitecture/MainActivity.java
+++ app/src/main/java/com/example/csh/androidarchitecture/MainActivity.java
@@ -1,13 +1,22 @@
 package com.example.csh.androidarchitecture;
 
+import android.arch.persistence.room.Room;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.csh.androidarchitecture.model.db.AppDatabase;
+import com.example.csh.androidarchitecture.model.db.User;
 
+public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        AppDatabase db = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "database-name").allowMainThreadQueries().build();
+        User user = new User();
+        user.setLastName("ss");
+        user.setFirstName("sss");
+        db.userDao().insertAll(user);
     }
 }
