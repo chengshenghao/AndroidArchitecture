254d8a91b8b047ae7fdbd2c011fe1dd7a53b97dc
chengshenghao <1151799682@qq.com>
2018-07-09 14:26:57 +0800

    new:增删查改功能的实现

--- app/src/main/java/com/example/csh/androidarchitecture/MainActivity.java
+++ app/src/main/java/com/example/csh/androidarchitecture/MainActivity.java
@@ -3,10 +3,13 @@ package com.example.csh.androidarchitecture;
 import android.arch.persistence.room.Room;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 import com.example.csh.androidarchitecture.model.db.AppDatabase;
 import com.example.csh.androidarchitecture.model.db.User;
 import com.example.csh.androidarchitecture.model.db.entity.Address;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
     @Override
@@ -15,15 +18,28 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         AppDatabase db = Room.databaseBuilder(getApplicationContext(),
                 AppDatabase.class, "database-name").allowMainThreadQueries().build();
-        User user = new User();
-        user.setLastName("ss");
-        user.setFirstName("sss");
-        Address address = new Address();
-        address.city = "天津";
-        address.postCode = 10;
-        address.state ="直辖市";
-        address.street = "华苑";
-        user.setAddress(address);
-        db.userDao().insertAll(user);
+        for (int i = 0; i < 3; i++) {
+            User user = new User();
+            user.setLastName("s" + i);
+            user.setFirstName("sss" + i);
+            Address address = new Address();
+            address.city = "天津";
+            address.postCode = 10;
+            address.state = "直辖市";
+            address.street = "华苑";
+            user.setAddress(address);
+            db.userDao().insertAll(user);
+            List<User> users =  db.userDao().getuid();
+            List<User> userstwo =  db.userDao().getfirstName("sss0");
+            db.userDao().update("新技术产业园区",2);
+            for (User usera : users) {
+                Log.i("sss", "onCreate: "+usera.getUid());
+                db.userDao().delete(usera);
+            }
+            for (User usera : userstwo) {
+                Log.i("sss", "onCreate: "+usera.getUid());
+                db.userDao().delete(usera);
+            }
+        }
     }
 }
--- app/src/main/java/com/example/csh/androidarchitecture/model/db/UserDao.java
+++ app/src/main/java/com/example/csh/androidarchitecture/model/db/UserDao.java
@@ -4,29 +4,40 @@ import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
+import android.arch.persistence.room.Update;
+
 import java.util.List;
 
 /*
-* 这个组件代表一个作为Data Access Objec的类或者接口。DAO是Room的主要组件，
-* 负责定义查询（添加或者删除等）数据库的方法。使用@Database注解的类必须包含一个0参数的，
-* 返回类型为@Dao注解过的类的抽象方法。Room会在编译时生成这个类的实现。
-* */
+ * 这个组件代表一个作为Data Access Objec的类或者接口。DAO是Room的主要组件，
+ * 负责定义查询（添加或者删除等）数据库的方法。使用@Database注解的类必须包含一个0参数的，
+ * 返回类型为@Dao注解过的类的抽象方法。Room会在编译时生成这个类的实现。
+ * */
 
 @Dao
-    public interface UserDao {
-        @Query("SELECT * FROM users")
-        List<User> getAll();
-     
-        @Query("SELECT * FROM users WHERE uid IN (:userIds)")
-        List<User> loadAllByIds(int[] userIds);
-     
-        @Query("SELECT * FROM users WHERE firstname LIKE :first AND "
-               + "last_name LIKE :last LIMIT 1")
-        User findByName(String first, String last);
-     
-        @Insert
-        void insertAll(User... users);
-     
-        @Delete
-        void delete(User user);
-    }
+public interface UserDao {
+    @Query("SELECT * FROM users")
+    List<User> getAll();
+
+    @Query("SELECT * FROM users WHERE uid = 11")
+    List<User> getuid();
+
+    @Query("SELECT * FROM users WHERE firstName LIKE :select")
+    List<User> getfirstName(String select);
+
+    @Query("update users set street=:stree where uid=:i")
+    void update(String stree,int i);
+
+    @Query("SELECT * FROM users WHERE uid IN (:userIds)")
+    List<User> loadAllByIds(int[] userIds);
+
+    @Query("SELECT * FROM users WHERE firstname LIKE :first AND "
+            + "last_name LIKE :last LIMIT 1")
+    User findByName(String first, String last);
+
+    @Insert
+    void insertAll(User... users);
+
+    @Delete
+    void delete(User user);
+}
