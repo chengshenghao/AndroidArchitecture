13b30645510b23b98b9ce30a7040377d87341d35
chengshenghao <1151799682@qq.com>
2018-07-09 10:16:45 +0800

    new:User添加tablename属性修改创建表名称

--- app/src/main/java/com/example/csh/androidarchitecture/model/db/User.java
+++ app/src/main/java/com/example/csh/androidarchitecture/model/db/User.java
@@ -2,14 +2,16 @@ package com.example.csh.androidarchitecture.model.db;
 
 import android.arch.persistence.room.ColumnInfo;
 import android.arch.persistence.room.Entity;
+import android.arch.persistence.room.Ignore;
 import android.arch.persistence.room.PrimaryKey;
+import android.graphics.Bitmap;
 
 /*
  * 这个组件代表一个持有数据库的一个表的类。对每一个entity，都会创建一个表来持有这些item。
  * 你必须在Database类中的entities数组中引用这些entity类。
  * entity中的每一个field都将被持久化到数据库，除非使用了@Ignore注解。
  * */
-@Entity
+@Entity(tableName = "users")//默认数据库名是类名，添加此注解可以重新设置类名称
 public class User {
     @PrimaryKey(autoGenerate = true)
     private int uid;
@@ -22,8 +24,11 @@ public class User {
 
     // Getters and setters are ignored for brevity,
     // but they're required for Room to work.
-
-
+    /*
+    * 不会创建相应列
+    * */
+    @Ignore
+    Bitmap picture;
     public int getUid() {
         return uid;
     }
--- app/src/main/java/com/example/csh/androidarchitecture/model/db/UserDao.java
+++ app/src/main/java/com/example/csh/androidarchitecture/model/db/UserDao.java
@@ -4,18 +4,23 @@ import android.arch.persistence.room.Dao;
 import android.arch.persistence.room.Delete;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.Query;
-
 import java.util.List;
+
+/*
+* 这个组件代表一个作为Data Access Objec的类或者接口。DAO是Room的主要组件，
+* 负责定义查询（添加或者删除等）数据库的方法。使用@Database注解的类必须包含一个0参数的，
+* 返回类型为@Dao注解过的类的抽象方法。Room会在编译时生成这个类的实现。
+* */
 
 @Dao
     public interface UserDao {
-        @Query("SELECT * FROM user")
+        @Query("SELECT * FROM users")
         List<User> getAll();
      
-        @Query("SELECT * FROM user WHERE uid IN (:userIds)")
+        @Query("SELECT * FROM users WHERE uid IN (:userIds)")
         List<User> loadAllByIds(int[] userIds);
      
-        @Query("SELECT * FROM user WHERE first_name LIKE :first AND "
+        @Query("SELECT * FROM users WHERE first_name LIKE :first AND "
                + "last_name LIKE :last LIMIT 1")
         User findByName(String first, String last);
      
